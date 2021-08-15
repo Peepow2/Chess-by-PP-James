fc8157807183d2c1711b213779de0dd258669b7e
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2021-08-15 20:55:45 +0700

    Add files via upload

---
+++ AutoClick POPCAT.py
@@ -0,0 +1,17 @@
+import pyautogui
+import time as tm
+
+def AutoClick(x, y, clicks, delay, tt):
+    t1 = tm.time();
+    t2 = -1
+    tm.sleep(3)
+    while(t2-t1 <= tt):
+        pyautogui.moveTo(x, y)
+        pyautogui.click(clicks=clicks)
+        tm.sleep(delay)
+        t2 = tm.time()
+    print("Finish");
+        
+# Driver Code
+tt = int(input("ให้กดเองกี่วินาที: " ))
+AutoClick(450, 530, 21, 0.05, tt)
